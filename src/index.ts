84a1c7339478263060e2fc41673e6ffd6c78fb8e
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-06-16 18:16:48 +0400

    :sparkles: Adds x-accordion (#3)
    * :sparkles: Adds Accordion Components
    
    * :bug: Fixes Group
    
    * :arrow_up: Upgrades devdeps

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import accordions from './headless/accordion';
 import { HeadlessComponent } from './headless/headless';
 import { panel } from './headless/panel';
 import { tabs } from './headless/tabs';
 
-export const UI: HeadlessComponent = (Alpine) => Alpine.plugin([panel, tabs]);
+export const UI: HeadlessComponent = (Alpine) =>
+  Alpine.plugin([accordions, panel, tabs]);
 
 export default UI;
